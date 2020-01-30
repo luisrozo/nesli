5a999a535ef3d072e6a9b421ba535d8eb8710658
luisrozo <luisgonzagarozo@gmail.com>
2020-01-30 17:19:47 +0100

    add and improve sign up/in views

--- login.php
+++ login.php
@@ -19,29 +19,18 @@
 
                 <div class="header">
                     <img src="assets/images/nesli-logo.png" title="Logo" alt="Site logo"  />
-                    <h3>Sign Up</h3>
+                    <h3>Sign In</h3>
                     <span>to continue to Nesli</span>
                 </div>
 
                 <form method="post">
-                    <input type="text" name="firstName" placeholder="First name" required>
-
-                    <input type="text" name="lastName" placeholder="Last name" required>
-
                     <input type="text" name="username" placeholder="Username" required>
-
-                    <input type="email" name="email" placeholder="Email" required>
-
-                    <input type="email" name="email2" placeholder="Confirm email" required>
-
                     <input type="password" name="password" placeholder="Password" required>
-
-                    <input type="password" name="password" placeholder="Confirm password" required>
 
                     <input type="submit" name="submitButton" value="Submit">
                 </form>
 
-                <a href="login.php" class="signInMessage">Already have an account? Sign in here.</a>
+                <a href="register.php" class="signInMessage">Need an account? Sign up here.</a>
 
             </div>
 
